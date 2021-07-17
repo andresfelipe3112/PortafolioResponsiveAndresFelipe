09022e15c961f22bf117a73fc788d648df5ee682
Andres Felipe <andresfelipedesarrollador@gmail.com>
2021-07-17 05:44:16 -0500

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,85 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 
+if (navToggle) {
+    navToggle.addEventListener("click", function () {
+        navMenu.classList.add("show-menu")
+        navToggle.style.visibility = "hidden"
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
+if (navClose) {
+    navClose.addEventListener("click", function () {
+        navMenu.classList.remove("show-menu")
+        navToggle.style.visibility = "visible";
+    })
+}
+
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const type = new Typed(".typed", {
+    strings: ["JavaScript Developer", "Frond-End Developer", "JavaScript Developer", "Profesional React.js Redux", "Web Desing"],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+    startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+    backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+    smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+    shuffle: false, // Alterar el orden en el que escribe las palabras.
+    backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+    loop: true, // Repetir el array de strings
+    loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+    showCursor: true, // Mostrar cursor palpitanto
+    cursorChar: '|', // Caracter para el cursor
+    contentType: 'html', // 'html' o 'null' para texto sin formato
+})
 
 
 /*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+console.log(skillsContent);
+console.log(skillsHeader);
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = "skills__content skills__close"
+    }
+
+    if (itemClass === "skills__content skills__close") {
+        this.parentNode.className = "skills__content skills__open"
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener("click", toggleSkills)
+})
+
+
 
 
 /*==================== QUALIFICATION TABS ====================*/
@@ -23,6 +90,19 @@
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
 
 /*==================== TESTIMONIAL ====================*/
 
@@ -30,10 +110,10 @@
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
